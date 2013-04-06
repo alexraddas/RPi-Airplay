2953bf047e3e8560e42c3f05b2d23c588fe1d1bf
alexraddas <alex.raddas@gmail.com>
2013-04-05 17:18:29 -0700

    Update AirPiInstall.sh

--- AirPiInstall.sh
+++ AirPiInstall.sh
@@ -1,15 +1,15 @@
-apt-get update -y
+apt-get update
 apt-get -y upgrade
 apt-get install git
 git clone https://github.com/alexraddas/RPi-Airplay /home/pi/AirPi
 cd /home/pi/AirPi
-apt-get install wpa-supplicant libao-dev libssl-dev libcrypt-openssl-rsa-perl libio-socket-inet6-perl libwww-perl avahi-utils$
-perl /home/pi/RPi-Airplay/perl-net-sdp/Build.PL
-sudo ./home/pi/RPi-Airplay/perl-net-sdp/Build
-sudo ./home/pi/RPi-Airplay/perl-net-sdp/Build test
-sudo ./home/pi/RPi-Airplay/perl-net-sdp/Build install
-make /home/pi/RPi-Airplay/shairport
-make /home/pi/RPi-Airplay/shairport/install
+apt-get -y install wpa-supplicant libao-dev libssl-dev libcrypt-openssl-rsa-perl libio-socket-inet6-perl libwww-perl avahi-utils$
+perl /home/pi/RPi-Airplay/perl/Build.PL
+sudo ./home/pi/RPi-Airplay/perl/Build
+sudo ./home/pi/RPi-Airplay/perl/Build test
+sudo ./home/pi/RPi-Airplay/perl/Build install
+make /home/pi/RPi-Airplay/airport
+make /home/pi/RPi-Airplay/airport/install
 chmod a+x /etc/init.d/shairport
 update-rc.d shairport defaults
 cp /home/pi/RPi-Airplay/config/asound.conf /etc
