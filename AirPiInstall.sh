b9e6610c2382d67b78d8067ecd5b96dc0a974667
alexraddas <alex.raddas@gmail.com>
2013-04-05 17:49:14 -0700

    Update AirPiInstall.sh

--- AirPiInstall.sh
+++ AirPiInstall.sh
@@ -3,14 +3,18 @@ apt-get -y upgrade
 apt-get install git
 git clone https://github.com/alexraddas/RPi-Airplay /home/pi/AirPi
 cd /home/pi/AirPi
-apt-get -y install wpa-supplicant libao-dev libssl-dev libcrypt-openssl-rsa-perl libio-socket-inet6-perl libwww-perl avahi-utils$
-perl /home/pi/RPi-Airplay/perl/Build.PL
-sudo ./home/pi/RPi-Airplay/perl/Build
-sudo ./home/pi/RPi-Airplay/perl/Build test
-sudo ./home/pi/RPi-Airplay/perl/Build install
-make /home/pi/RPi-Airplay/airport
-make /home/pi/RPi-Airplay/airport/install
+apt-get -y install libcrypt-ssleay-perl wpasupplicant libao-dev libssl-dev libcrypt-openssl-rsa-perl libio-socket-inet6-perl libwww-perl avahi-utils
+cd /home/pi/AirPi/perl
+perl Build.PL
+./Build
+./Build test
+./Build install
+cd /home/pi/AirPi/airplay
+make
+make install
+cp shairport.init.sample /etc/init.d/shairport
 chmod a+x /etc/init.d/shairport
 update-rc.d shairport defaults
-cp /home/pi/RPi-Airplay/config/asound.conf /etc
-cp /home/pi/RPi-Airplay/config/wpa-supplicant.conf /etc/wpa-supplicant
+cp /home/pi/AirPi/config/asound.conf /etc
+cp /home/pi/AirPi/config/wpa_supplicant.conf /etc/wpa-supplicant
+cp /home/pi/AirPi/config/interfaces /etc/network
